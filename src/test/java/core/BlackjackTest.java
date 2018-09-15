3456760d5ab37fd58466410471d275fa75054bf4
Janielle Scarlett <janiellescarlett@cmail.carleton.ca>
2018-09-15 13:33:07 -0400

    Updated tests : added method testShuffle()

--- src/test/java/core/BlackjackTest.java
+++ src/test/java/core/BlackjackTest.java
@@ -1,5 +1,10 @@
 package core;
 
+import java.awt.List;
+import java.util.ArrayList;
+
+import org.junit.validator.ValidateWith;
+
 import junit.framework.TestCase;
 
 public class BlackjackTest extends TestCase{
@@ -12,7 +17,7 @@ public class BlackjackTest extends TestCase{
 		Card drawedCard = deck.drawCard();
 		aHand.add(drawedCard);	
 		
-		assertEquals(null, deck.findCard(drawedCard));			
+		assertFalse(deck.findCard(drawedCard));			
 	}
 	
 	public void testHand() {
@@ -22,7 +27,15 @@ public class BlackjackTest extends TestCase{
 		
 		aHand.add(drawedCard);
 
-		assertEquals(null, aHand.findCard(drawedCard));
+		assertTrue(aHand.findCard(drawedCard));		
+	}
+	
+	public void testShuffle() {
+		Deck deck1 = new Deck();
+		Deck deck2 = new Deck();
+		
+		assertFalse(deck1.same(deck2));	
+		assertTrue(deck1.same(deck1));
 		
 	}
 }
